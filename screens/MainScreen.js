69c0ab00d1a6f970b592cab160be90d082d63189
X3R02 <x3r02mail@gmail.com>
2021-01-20 18:06:41 -0600

    Añadiendo los componentes necesarios

---
+++ screens/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+const MainScreen = () => {
+    return (
+        <ScrollView>
+            
+        </ScrollView>
+    );
+};
+
+export default MainScreen;
